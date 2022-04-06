3fb1c69db23241af667be9731c820ed36e111fd2
Ofer <ozadaka@habana.ai>
2022-04-06 15:15:10 +0300

    changes

--- printfy.go
+++ printfy.go
@@ -7,31 +7,6 @@ import (
 
 type any = interface{}
 
-func Printfy(stri string, args ...any) {
-	arg := false
-	str := ""
-
-	for i := 0; i < len(stri); i++ {
-		switch string(stri[i]) {
-		case "{":
-			arg = true
-			continue
-		case "}":
-			arg = false
-			continue
-		}
-
-		if !arg {
-			str = str + string(stri[i])
-		} else {
-			args_index, _ := strconv.Atoi(string(stri[i]))
-			str = str + fmt.Sprintf("%v", args[args_index])
-			args_index++
-		}
-	}
-	fmt.Print(str)
-}
-
 func Stringfy(stri string, args ...any) string {
 	arg := false
 	str := ""
@@ -55,4 +30,19 @@ func Stringfy(stri string, args ...any) string {
 		}
 	}
 	return str
+}
+
+func Printfy(stri string, args ...any) {
+	str := Stringfy(stri, args)
+	fmt.Print(str)
+}
+
+func getHighestIndex(stri string) int {
+	var index int
+	for i := 0; i < len(stri); i++ {
+		if string(stri[i]) == "{" {
+			index = i
+		}
+	}
+	return index
 }
